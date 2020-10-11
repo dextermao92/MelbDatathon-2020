1a43c9bbb5e09b526a8129af0403bd64e3fc9ab6
Dexter Mao <dextermao@Dexters-MacBook-Pro.local>
2020-10-11 23:51:15 +1100

    Updates

---
+++ precache-manifest.0e3261da3690eb6079ba3f7b07cf6ce0.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6b33b8a40f0034545ff74d6e7e7b9358",
+    "url": "/MelbDatathon-2020/index.html"
+  },
+  {
+    "revision": "b6aef0d87723ac98bcdf",
+    "url": "/MelbDatathon-2020/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "9d450a65a2ea0ab6d358",
+    "url": "/MelbDatathon-2020/static/js/2.e5f0e2ab.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/MelbDatathon-2020/static/js/2.e5f0e2ab.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b6aef0d87723ac98bcdf",
+    "url": "/MelbDatathon-2020/static/js/main.d56e35e1.chunk.js"
+  },
+  {
+    "revision": "c8028ce0c3ccafb21145",
+    "url": "/MelbDatathon-2020/static/js/runtime-main.42bc9f8e.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/MelbDatathon-2020/static/media/logo.5d5d9eef.svg"
+  }
+]);
